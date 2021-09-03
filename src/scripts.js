ed45ec320d18608eac24287aae65de3485e9f378
Joshua Horner <lesliewbusiness@gmail.com>
2021-09-03 17:59:05 -0600

    Add function to filter selected recipes

--- src/scripts.js
+++ src/scripts.js
@@ -9,6 +9,7 @@ import star from './data/assets/star.svg';
 import {userData} from './data/users.js';
 import {recipeData} from './data/recipes.js';
 
+// BUTTONS & SECTIONS //
 
 const allRecipeGrid = document.querySelector('#allRecipeGrid');
 const mainContent = document.querySelector('#mainContent');
@@ -24,6 +25,12 @@ const addRecipeForm = document.querySelector('#addRecipeForm');
 const loginPopup = document.querySelector('#loginPopup');
 const loginButton = document.querySelector('#loginButton');
 
+// FILTER CHECKBOXES && SEARCH ARRAY //
+
+const filters = document.querySelector('#filters');
+let filterSelection = [];
+
+// EVENT LISTENERS //
 
 searchSubmitBtn.addEventListener('click', searchByName);
 allRecipes.addEventListener('click', viewAllRecipes);
@@ -32,6 +39,18 @@ homeButton.addEventListener('click', showHomeView);
 addRecipeButton.addEventListener('click', showRecipeForm);
 loginButton.addEventListener('click', showLogin);
 savedRecipesButton.addEventListener('click', viewAllRecipes);
+filters.addEventListener('click', filterRecipes);
+
+// MAIN FUNCTIONS //
+
+function filterRecipes() {
+  // event.preventDefault();
+  if (event.target.value) {
+    filterSelection.push(event.target.value);
+    console.log(filterSelection);
+    searchByTag(recipeData, filterSelection);
+  }
+}
 
 function viewAllRecipes() {
   const recipeRepo = new RecipeRepository(recipeData);
@@ -147,24 +166,28 @@ function joinToString(array){
 
 
 function searchByTag(recipesArray, searchTags){
-  let indexMatchAllStrings
+  let indexMatchAllStrings;
+  let indexMatchAllSearchTags;
   let returnedArr = []
   let filteredArray = recipesArray.reduce((acc, recipe) => {
       let tagsString = joinToString(recipe.tags);
       let numOfTags = searchTags.length;
+      // console.log(searchTags);
       let testTags = searchTags.reduce((acc, tag) => {
         if (tagsString.includes(tag)){
           acc++;
         }
         return acc;
       }, 0)
-      console.log(testTags===numOfTags)
+      // console.log(testTags===numOfTags)
       indexMatchAllSearchTags = (numOfTags===testTags)
+      // console.log('NUMBER: ', numOfTags, 'TAGS: ', testTags)
       if (indexMatchAllSearchTags){
         returnedArr.push(recipe);
       }
 
     }, []);
+    populateCards(returnedArr);
   return returnedArr;
 };
 
