18ef7da46521e45c025f26296daea072f23477fb
Renee Swick-Ziller <reneeswick@gmail.com>
2021-09-04 23:26:38 -0600

    Debug the favorite a recipe feature and views

--- src/scripts.js
+++ src/scripts.js
@@ -10,8 +10,9 @@ import star from './data/assets/star.svg';
 import {usersData} from './data/users.js';
 import {recipeData} from './data/recipes.js';
 
-// BUTTONS & SECTIONS //
-
+////////////////// GLOBAL VARIABLES //////////////////////////////////
+var currentUser;
+////////////////// QUERY SELECTORS ///////////////////////////////////
 const allRecipeGrid = document.querySelector('#allRecipeGrid');
 const mainContent = document.querySelector('#mainContent');
 const allRecipes = document.querySelector('#allRecipesButton');
@@ -23,10 +24,8 @@ const recipeFormTitle = document.querySelector('#recipeFormTitle');
 const recipeFormImage = document.querySelector('#recipeFormImage');
 const recipeFormIngredient = document.querySelector('#recipeFormIngredient');
 const unitSelection = document.querySelector('#unitSelection');
-
-
+////////////////// BUTTONS & FORMS ///////////////////////////////////
 const ingredientAmount = document.querySelector('#ingredientAmount');
-
 const homeButton = document.querySelector('#homeButton');
 const savedRecipesButton = document.querySelector('#savedRecipesButton');
 const addRecipeButton = document.querySelector('#addRecipeButton');
@@ -37,19 +36,15 @@ const plusButton = document.querySelector('#plusButtonContainer');
 const homeFavoriteStar1 = document.querySelector('#favoriteStar1')
 const homeFavoriteStar2 = document.querySelector('#favoriteStar2')
 const homeFavoriteStar3 = document.querySelector('#favoriteStar3')
-var currentUser;
-// const submitRecipeButton = document.querySelector('#submitRecipe');
 const submitRecipeButton = document.querySelector('#submitRecipe');
 const addIngredientButton = document.querySelector('#plusButtonContainer');
 
-// FILTER CHECKBOXES && SEARCH ARRAY //
-
+////////////////////// FILTER CHECKBOXES && SEARCH ARRAY /////////////////////
 const filters = document.querySelector('#filters');
 let filterSelection = [];
 let addedIngredients = [];
 
-// EVENT LISTENERS //
-
+/////////////////////// EVENT LISTENERS ////////////////////////////////////////
 searchSubmitBtn.addEventListener('click', searchByName);
 allRecipes.addEventListener('click', viewAllRecipes);
 contentContainer.addEventListener('click', getDirections);
@@ -61,22 +56,19 @@ addRecipeButton.addEventListener('click', showRecipeForm);
 loginButton.addEventListener('click', showLogin);
 savedRecipesButton.addEventListener('click', function(){populateCards(currentUser.favoriteRecipes)});
 filters.addEventListener('click', filterRecipes);
-// plusButton.addEventListener('click', addIngredient);
-// submitRecipeButton.addEventListener('click', addNewRecipe);
 plusButton.addEventListener('click', addIngredient);
 submitRecipeButton.addEventListener('click', addNewRecipe);
 addIngredientButton.addEventListener('click', addIngredient);
 
-// MAIN FUNCTIONS //
+//////////////////// FUNCTIONS ///////////////////////////////////////
 
 function filterRecipes() {
-  // event.preventDefault();
   if (event.target.value) {
     filterSelection.push(event.target.value);
     console.log(filterSelection);
     searchByTag(recipeData, filterSelection);
   }
-}
+};
 
 function addIngredient() {
   let ingredient = recipeFormIngredient.value;
@@ -86,11 +78,11 @@ function addIngredient() {
   recipeFormIngredient.value = null;
   unitSelection.value = null;
   ingredientAmount.value = null;
-}
+};
 
 function generateRandomNumber() {
   return Math.floor(Math.random()*90000) + 10000;
-}
+};
 
 function addNewRecipe() {
   let titleField = recipeFormTitle.value;
@@ -99,11 +91,9 @@ function addNewRecipe() {
   let unitField = unitSelection.value;
   addIngredient();
   let newRecipe = new Recipe({id: generateRandomNumber(), name: titleField, image: imageField, ingredients: [addedIngredients]});
-
-  console.log(newRecipe);
   recipeData.push(newRecipe);
   addedIngredients = [];
-}
+};
 
 function viewAllRecipes() {
   hide(recipeGrid);
@@ -111,11 +101,10 @@ function viewAllRecipes() {
   show(allRecipeGrid);
 
   let recipeRepo = new RecipeRepository(recipeData);
-  console.log(recipeRepo)
   allRecipeGrid.innerHTML = ""
   const viewAllRecipes = recipeRepo.recipeData.reduce((acc, recipe) => {
     allRecipeGrid.innerHTML +=
-      `<article class="mini-recipe" id="card-${recipe.id}">
+      `<article class="mini-recipe" id="${recipe.id}">
        <img src= "${recipe.image}" alt= "${recipe.name}">
        <p>${recipe.name}</p>
        <button type="favoriteStar" name="favoriteStar" class="favorite-star" id="faveBtn-${recipe.id}"></button>
@@ -130,24 +119,27 @@ function showHomeView() {
   show(recipeGrid);
   hide(addRecipeForm);
   hide(allRecipeGrid);
-  // restoreDefaultContentContainer();
 };
 
 function showRecipeForm() {
   show(addRecipeForm);
   hide(recipeGrid);
   hide(allRecipeGrid);
-}
+};
 
 function showLogin() {
   loginPopup.classList.toggle('hidden');
-}
+};
 
 
-function getDirections(targetID){
-  allRecipeGrid.classList.add('hidden');
+function getDirections(event){
+  if(event.target.classList.contains('favorite-star')){
+    addToFavorites();
+    return
+  };
 
-  // let targetID = event.target.closest('.mini-recipe').id;
+  allRecipeGrid.innerHTML = "";
+  let targetID = event.target.closest('.mini-recipe').id;
   let newRecipeInfo = recipeData.find(recipe => recipe.id === Number(targetID));
   let selectedRecipe = new Recipe(newRecipeInfo);
 
@@ -173,17 +165,13 @@ function getDirections(targetID){
   }, '')
 
   let fullRecipe =
-    `<section class="full-recipe-container" id="fullRecipeContainer">
-      <h3 class= "full-recipe"> ${selectedRecipe.name}</h3>
-      <img src= "${selectedRecipe.image}" alt="${selectedRecipe.name}">
-      <p class= "ingredients">${ingredients.split(/[ ,]+/).join(' ,')}</p>
-      <p class= "cost">${selectedRecipe.returnCostEstimation()}</p>
-      <p class= "instructions">${instructions}</p>
-    </section>`;
+    `<h3 class= "full-recipe"> ${selectedRecipe.name}</h3>
+    <img src= "${selectedRecipe.image}" alt="${selectedRecipe.name}">
+    <p class= "ingredients">${ingredients.split(/[ ,]+/).join(' ,')}</p>
+    <p class= "cost">${selectedRecipe.returnCostEstimation()}</p>
+    <p class= "instructions">${instructions}</p>`;
 
-
-  return contentContainer.innerHTML = fullRecipe
-
+  allRecipeGrid.innerHTML = fullRecipe
 };
 
 
@@ -191,17 +179,16 @@ function searchByName(){
   if(searchFieldInput.value ===""){
     popupMessage("Please enter a search term!", 1000, "red")
     return
-  }
+  };
   let recipeRepo = new RecipeRepository(recipeData);
   let filteredRecipes = recipeRepo.filterByName(searchFieldInput.value)
   if (filteredRecipes.length === 0){
 
     popupMessage("No results found! Sorry!", 1000, "red")
     return
-  }
+  };
   populateCards(filteredRecipes)
-
-}
+};
 
 function populateCards(arr){
   show(allRecipeGrid);
@@ -210,20 +197,14 @@ function populateCards(arr){
   allRecipeGrid.innerHTML = ""
   const recipeCard = arr.reduce((acc, recipe) => {
     allRecipeGrid.innerHTML +=
-      `<article class="mini-recipe" id="card-${recipe.id}">
+      `<article class="mini-recipe" id="${recipe.id}">
        <img src= "${recipe.image}" alt= "${recipe.name}">
        <p>${recipe.name}</p>
-       <button type="favoriteStar" name="favoriteStar" class="favorite-star" id="faveBtn-${recipe.id}"></button>
+       <button type="favoriteStar" name="favoriteStar" class="favorite-star" id="fave-${recipe.id}"></button>
        </article>`
 
-      let recipeIndex = recipeRepo.recipeData.indexOf(recipe)
-      let eachCard =  document.querySelector(`#card-${recipe.id}`)
-      eachCard.addEventListener("click", ()=> getDirections(Number(recipe.id)))
-      let eachButton = document.querySelector(`#faveBtn-${recipe.id}`)
-      eachButton.addEventListener("click", ()=>{selectFavoriteRecipe(recipeIndex)})
       return acc;
     }, []);
-    console.log(currentUser)
 };
 
 function searchData(input) {
@@ -233,18 +214,11 @@ function searchData(input) {
 
 function setUserData(){
   let user = new UserData()
-
-
-
-  // if the user login matches the user.name....
-  //...then the app populates with that user's info
 };
-
 
 function joinToString(array){
   return array.join(" ")
-}
-
+};
 
 function searchByTag(recipesArray, searchTags){
   let indexMatchAllStrings
@@ -270,10 +244,11 @@ function searchByTag(recipesArray, searchTags){
 
 function hide(element){
   element.classList.add('hidden');
-}
+};
+
 function show(element){
   element.classList.remove('hidden')
-}
+};
 
 function popupMessage(message, timeInMS, color = "gold"){
   let popupContainer = document.querySelector('#popup')
@@ -285,14 +260,24 @@ function popupMessage(message, timeInMS, color = "gold"){
       popupContainer.classList.remove(`${color}-popup`)
       hide(popupContainer);
   }, timeInMS)
-}
-
-
-function selectFavoriteRecipe(index) {
-  currentUser.toggleItemInArray('favoriteRecipes', recipeData[index]);
-  console.log(currentUser.favoriteRecipes);
 };
 
+
+// function selectFavoriteRecipe(index) {
+//   currentUser.toggleItemInArray('favoriteRecipes', recipeData[index]);
+//   console.log(currentUser.favoriteRecipes);
+// };
+
+function addToFavorites(){
+  const recipeRepo = new RecipeRepository(recipeData);
+  let recipeID = event.target.closest('.mini-recipe').id;
+  let fullRecipe = recipeRepo.recipeData.find(recipe => recipe.id === Number(recipeID));
+  let selectedRecipe = new Recipe(fullRecipe);
+  let index = recipeRepo.recipeData.indexOf(fullRecipe);
+  currentUser.toggleItemInArray('favoriteRecipes', recipeData[index]);
+
+  console.log(currentUser.favoriteRecipes);
+};
 
 
 function generateRandomUser() {
@@ -300,65 +285,6 @@ function generateRandomUser() {
   const user = usersData[randomUser];
   let userDataValue = new UserData(user)
   currentUser = userDataValue
-  console.log(currentUser)
-
-};
-
-function restoreDefaultContentContainer(){
-  contentContainer.innerHTML = `<section class="content-container" id="contentContainer">
-    <section class="main-content" id="mainContent">
-      <section class="recipe-grid" id="recipeGrid">
-        <article class="recipe">
-          <div class="meal-image">
-            <img src="images/pancakes.svg" alt="meal image" class="image">
-          </div>
-          <div class="recipe-content">
-            <div class="favorite-star"></div>
-            <img class="star" src="images/star-active.svg" alt="favorite">
-            <div class="recipe-info">
-              <h2>Dish Name</h2>
-              <div class="ingredient-section">
-                <h4>Ingredients</h4>
-              <section class="ingredient-info"> </section>
-              </div>
-            </div>
-          </div>
-        </article>
-        <article class="recipe">
-          <div class="meal-image">
-            <img src="images/pancakes.svg" alt="meal image" class="image">
-          </div>
-          <div class="recipe-content">
-            <div class="favorite-star"></div>
-            <img class="star" src="images/star-active.svg" alt="favorite">
-            <div class="recipe-info">
-              <h2>Dish Name</h2>
-              <div class="ingredient-section">
-                <h4>Ingredients</h4>
-              <section class="ingredient-info"></section>
-              </div>
-            </div>
-          </div>
-        </article>
-        <article class="recipe">
-          <div class="meal-image">
-            <img src="images/pancakes.svg" alt="meal image" class="image">
-          </div>
-          <div class="recipe-content">
-            <div class="favorite-star"></div>
-            <img class="star" src="images/star-active.svg" alt="favorite">
-            <div class="recipe-info">
-              <h2>Dish Name</h2>
-              <div class="ingredient-section">
-                <h4>Ingredients</h4>
-              <section class="ingredient-info"></section>
-              </div>
-            </div>
-          </div>
-        </article>
-      </section>
-    </section>
-  </section>`
 };
 
 generateRandomUser();
